d70cf1fd29e86f6d6f73efa948529745088c4013
Natalie Mall <nataliemall@campus-101-140.ucdavis.edu>
2018-05-14 17:04:59 -0700

    Converting '/' to '_' in column names attempt 1

--- calPIP_all_crops_2015.py
+++ calPIP_all_crops_2015.py
@@ -10,6 +10,7 @@ import os
 import pdb
 import pandas as pd
 import seaborn as sns
+import re 
 from mpl_toolkits.basemap import Basemap
 from tqdm import tqdm  # for something in tqdm(something something):
 
@@ -22,14 +23,43 @@ calPIP_2015_data = pd.read_csv('calPIP_includes_crop_areas/calPIP2015.csv', sep=
 # calPIP_2015_data.COMTRS
 
 crop_list = calPIP_2015_data.SITE_NAME.unique()
-crop_list = crop_list[0:10]
+pdb.set_trace()
+crop_list = crop_list #[0:20]                      # works with 20, but not with 166... (because '/' is included in column names)
+
+
+def escapeTexString(string): # Returns TeX-friendly string
+    rep = { # define desired replacements in this dictionary (mapping)
+         '/': '_',
+         # '%': '\\%',
+         # '#': '\\#',
+         # '_': '\\_',
+         # '{': '\\{', # REGEX Special
+         # '}': '\\}', # REGEX Special
+         # '~': '\\char"007E{}', # LaTeX Special
+         # '$': '\\$', # REGEX Special
+         # '\\': '\\char"005C{}', # REGEX/LaTeX Special
+         # '^': '\\char"005E{}', # REGEX/LaTeX Special
+         # '"': '\\char"FF02{}'
+        }
+    # use these two lines to do the replacement (could be shortened to one line)
+    pattern = re.compile("|".join(map(re.escape,rep.keys()))) # Create single pattern object (key to simultaneous replacement)
+    new_string = pattern.sub(lambda match: rep[match.group(0)], string)
+    return new_string
+
+crop_list_fixed = escapeTexString(crop_list)
+
+
+
+# my_string = my_string.replace(to_replace = ['A','B','C','D','E'], value = ['1','2','3','4','5'])
+pdb.set_trace()
+
 
 # crop_labels_complete = crop_list[1:10, :]
 # column_list = np.zeros(len(calPIP_2015_data.SITE_NAME.unique()))
 column_list = []
 iter = 0 
 for crop_type in tqdm(crop_list): 
-    test3 = crop_type[0:10] + ' acres'
+    test3 = crop_type[0:15] + ' acres'
     # column_list[iter] = test3
     column_list.append(test3)
     iter = iter + 1
@@ -56,8 +86,8 @@ for crop_type in tqdm(crop_list):
     crop_df = pd.DataFrame({"COMTRS_alf": [COMTRS_list] })
 
     array_zeros = np.zeros([no_COMTRS, 1])  # array of the length of COMTRS for alfalfa
-    crop2_df = pd.DataFrame(array_zeros, index = [COMTRS_list], columns = [ crop_type[0:10] + ' acres'] )   # change column label to each type of crop 
-    crop_column = crop_type[0:10] + ' acres'
+    crop2_df = pd.DataFrame(array_zeros, index = [COMTRS_list], columns = [ crop_type[0:15] + ' acres'] )   # change column label to each type of crop 
+    crop_column = crop_type[0:15] + ' acres'
 
     test1 = crop2_df[lambda crop2_df: crop2_df.columns[0]]
     # pdb.set_trace()
@@ -96,8 +126,8 @@ for crop_type in tqdm(crop_list):
     # crop4_df['ALFALFA (F acres'] = crop2_df['ALFALFA (F acres'].loc['10M10S13E34']   # YAY KEEP FOR FUTURE REF
 
     # crop4_df['ALFALFA (F acres'] = crop2_df['ALFALFA (F acres'].loc[crop2_df.index]   # YAY KEEP FOR FUTURE REF
-    pdb.set_trace()
-    crop4_df[crop_type[0:10] + ' acres'] = crop2_df[crop_type[0:10] + ' acres'].loc[crop2_df.index]  # Puts the individual crop acreage list into the overall dataframe crop4_df 
+    # pdb.set_trace()
+    crop4_df[crop_type[0:15] + ' acres'] = crop2_df[crop_type[0:15] + ' acres'].loc[crop2_df.index]  # Puts the individual crop acreage list into the overall dataframe crop4_df 
 
     crop_test = crop4_df.loc[crop4_df.index == '10M10S13E34']
     crop4_df.loc['10M13S13E28']
@@ -110,13 +140,13 @@ for crop_type in tqdm(crop_list):
 
     save_acres = 1     # Saves each crop's data for each COMTRS 
     if save_acres == 1:
-        crop_label = crop_type[0:10] + 'acres'
+        crop_label = crop_type[0:15] + 'acres'
 
 
 
         crop3_df = crop2_df.reset_index()
         crop3_df.columns = ['COMTRS', crop_label]
-        crop_file_name = crop_label + '.csv'
+        crop_file_name = crop_label + '.csv'     # transform these names by replacing '/' with '_'
         # crop3_df.to_csv('crop_label.csv',header = True, na_rep = '0', index = False)    # how do I change the name of the csv file depending on the crop type? 
         
         path='/Users/nataliemall/Box Sync/herman_research_box/calPIP_crop_acreages'
@@ -126,7 +156,7 @@ for crop_type in tqdm(crop_list):
 
         crop3_df.to_csv(os.path.join(path, crop_file_name), header = True, na_rep = '0', index = False)   # HOW SAVE? 
 
-# pdb.set_trace()
+pdb.set_trace()
 
 crop5_df = crop4_df.reset_index()
 
